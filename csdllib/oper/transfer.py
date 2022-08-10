9ac95bc433118719b6f7c947542141b4580f589b
GeorgiosBritzolakisNOAAFederal <Georgios.Britzolakis@noaa.gov>
2022-08-10 15:49:51 +0000

    Mergin code fixes to comply with the new coops website for requesting the station data. Added code functionality to retry in case the station download did not work. Includes fixes to run with SCHISM model data.

--- csdllib/oper/transfer.py
+++ csdllib/oper/transfer.py
@@ -6,6 +6,53 @@ import urllib.request
 import uuid
 import ssl
 from csdllib.oper.sys import msg
+from pathlib import Path
+
+import time
+from functools import wraps
+
+
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
+    """Retry calling the decorated function using an exponential backoff.
+
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+    :param ExceptionToCheck: the exception to check. may be a tuple of
+        exceptions to check
+    :type ExceptionToCheck: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay
+        each retry
+    :type backoff: int
+    :param logger: logger to use. If None, print
+    :type logger: logging.Logger instance
+    """
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck as e:
+                    message = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                    if logger:
+                        logger.warning(message)
+                    else:
+                        print(message)
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+
+        return f_retry  # true decorator
+
+    return deco_retry
 
 #==============================================================================
 def download (remote, local):
@@ -65,10 +112,11 @@ def readlines (remote, verbose=False, tmpDir=None, tmpFile=None):
     lines  = fp.readlines()
     fp.close()
     os.remove( tmpFile )
-        
+            
     return lines
 
 #==============================================================================
+@retry(Exception, tries=100, delay=10, backoff=1)
 def readlines_ssl (remote, verbose=False, tmpDir=None, tmpFile=None):
     """
     Deals with expired SSL certificate issue.
@@ -104,6 +152,16 @@ def readlines_ssl (remote, verbose=False, tmpDir=None, tmpFile=None):
 
 #==============================================================================
 def upload(localFile, userHost, remoteFolder):
+    #Remove the old Files before copying
+    remoteFile = Path(localFile).name
+    remoteFilePath = os.path.join(remoteFolder, remoteFile)
+    cmd = 'ssh -q ' + userHost + " 'rm -rf " + remoteFilePath + "'"
+    if os.system(cmd) == 0:
+        msg('info', 'executed ' + cmd)
+    else:
+        msg('error', 'failed to execute ' + cmd)
+
+    #Copy the new Files
     cmd = 'scp -q ' + localFile + ' ' + userHost + ':' + remoteFolder
     if os.system(cmd) == 0:
         msg('info', 'executed ' + cmd)
